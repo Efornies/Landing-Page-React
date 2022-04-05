143213459a924c9f709718239283454da390d20a
Efornies <eleforbel@gmail.com>
2022-04-05 09:59:28 +0000

    navbar y jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Cards from "./cards.jsx";
 import Footer from "./footer.jsx";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
 
 const Home = () => {
 	return (
 		<div>
+			<Navbar />
+			<Jumbotron />
 			<Cards />
 			<Footer />
-
 			{/* 			
 			<Footer /> */}
 		</div>
